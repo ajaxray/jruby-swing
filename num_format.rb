0eb4b256bd13739608b84c4bd499334c42c655de
Nickolay Kondratenko <devmarkup@gmail.com>
2013-07-26 20:34:49 +0300

    remove semicolons

--- num_format.rb
+++ num_format.rb
@@ -15,28 +15,28 @@ class NumberConverter < JFrame
   def initialize
     super('Number Format Converter')
 
-    main = JPanel.new;
+    main = JPanel.new
     main.set_layout(GridLayout.new(3,3,2,2))
 
-    main.add(JLabel.new("Convert From : ", JLabel::RIGHT));
-    main.add(cmbFrom = JComboBox.new);
+    main.add(JLabel.new("Convert From : ", JLabel::RIGHT))
+    main.add(cmbFrom = JComboBox.new)
 
-    main.add(input = JTextField.new(15));
+    main.add(input = JTextField.new(15))
 
-    main.add(JLabel.new("Convert To : ", JLabel::RIGHT));
-    main.add(cmbTo = JComboBox.new);
-    main.add(result = JTextField.new(15));
-    result.set_editable(false);
+    main.add(JLabel.new("Convert To : ", JLabel::RIGHT))
+    main.add(cmbTo = JComboBox.new)
+    main.add(result = JTextField.new(15))
+    result.set_editable(false)
 
     main.add(JLabel.new(" ")); # Leave 1 grid cell blank
 
-    main.add(btn = JButton.new("CONVERT"));
+    main.add(btn = JButton.new("CONVERT"))
     btn.add_action_listener do
       format_from = cmbFrom.get_selected_item.to_s
       format_to = cmbTo.get_selected_item.to_s
       number = input.get_text
 
-      result.set_text(convert(format_from, format_to, number));
+      result.set_text(convert(format_from, format_to, number))
     end
 
     NUM_FORMATS.keys.each do |format|
@@ -44,19 +44,19 @@ class NumberConverter < JFrame
       cmbTo.add_item format
     end
 
-    get_content_pane().add("Center", main);
-    get_content_pane().add("South", JLabel.new("<html><br /><p style=\"color: red; font-size:8px\">Don't use fractions</p></html>", JLabel::CENTER));
+    get_content_pane().add("Center", main)
+    get_content_pane().add("South", JLabel.new("<html><br /><p style=\"color: red; font-size:8px\">Don't use fractions</p></html>", JLabel::CENTER))
 
-    set_size(400,140);
-    set_visible(true);
-    set_default_close_operation(JFrame::EXIT_ON_CLOSE);
+    set_size(400,140)
+    set_visible(true)
+    set_default_close_operation(JFrame::EXIT_ON_CLOSE)
   end
 
   def convert(format_from, format_to, number)
     radix = NUM_FORMATS[format_from]
 
     #Convert input to an int
-    number = number.to_i(radix);
+    number = number.to_i(radix)
 
     #Convert the int to specified format
     number.to_s(NUM_FORMATS[format_to]).upcase
