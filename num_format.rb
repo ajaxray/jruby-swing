d2990bc34106aaf794bb0930d89f70186babf977
Nickolay Kondratenko <devmarkup@gmail.com>
2013-07-26 20:33:03 +0300

    refactor convert function

--- num_format.rb
+++ num_format.rb
@@ -10,6 +10,7 @@ import javax.swing.JTextField
 import java.awt.GridLayout
 
 class NumberConverter < JFrame
+  NUM_FORMATS = {"Decimal" => 10, "Binary" => 2, "HexaDecimal" => 16, "Octal" => 8}
 
   def initialize
     super('Number Format Converter')
@@ -18,27 +19,29 @@ class NumberConverter < JFrame
     main.set_layout(GridLayout.new(3,3,2,2))
 
     main.add(JLabel.new("Convert From : ", JLabel::RIGHT));
-    main.add(@cmbFrom = JComboBox.new);
+    main.add(cmbFrom = JComboBox.new);
 
-    main.add(@input = JTextField.new(15));
+    main.add(input = JTextField.new(15));
 
     main.add(JLabel.new("Convert To : ", JLabel::RIGHT));
-    main.add(@cmbTo = JComboBox.new);
-    # 'result' is not being accessed from outside this function. So, leaving in function scope. 
-      # not declearing with @
+    main.add(cmbTo = JComboBox.new);
     main.add(result = JTextField.new(15));
     result.set_editable(false);
 
     main.add(JLabel.new(" ")); # Leave 1 grid cell blank
+
     main.add(btn = JButton.new("CONVERT"));
-    btn.add_action_listener do |evt|
-      result.set_text(convert.upcase);
+    btn.add_action_listener do
+      format_from = cmbFrom.get_selected_item.to_s
+      format_to = cmbTo.get_selected_item.to_s
+      number = input.get_text
+
+      result.set_text(convert(format_from, format_to, number));
     end
 
-    @num_formats = {"Decimal" => 10, "Binary" => 2, "HexaDecimal" => 16, "Octal" => 8}
-    @num_formats.keys.each do |format|
-      @cmbFrom.add_item format
-      @cmbTo.add_item format
+    NUM_FORMATS.keys.each do |format|
+      cmbFrom.add_item format
+      cmbTo.add_item format
     end
 
     get_content_pane().add("Center", main);
@@ -49,18 +52,16 @@ class NumberConverter < JFrame
     set_default_close_operation(JFrame::EXIT_ON_CLOSE);
   end
 
-  def convert
-    format_from = @cmbFrom.get_selected_item.to_s
-    radix = @num_formats[format_from]
+  def convert(format_from, format_to, number)
+    radix = NUM_FORMATS[format_from]
 
     #Convert input to an int
-    num = @input.get_text.to_i(radix);
+    number = number.to_i(radix);
 
     #Convert the int to specified format
-    format_to = @cmbTo.get_selected_item.to_s
-    num.to_s(@num_formats[format_to])
+    number.to_s(NUM_FORMATS[format_to]).upcase
   end
 
 end
 
-num_conv = NumberConverter.new
+NumberConverter.new
